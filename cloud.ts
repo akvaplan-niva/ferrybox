2a88f987753b264a08240e3c16dae7782284d812
cnrdh <cnrdh@users.noreply.github.com>
2022-11-11 12:24:27 +0100

    new files

---
+++ cloud.ts
@@ -0,0 +1,20 @@
+import { isGregorianISODate } from "./time.ts";
+import { ferryboxOptions } from "./config.ts";
+
+// Relative cloud path for 1-day files
+// Path for daily (p1d) file in dataVersion 0 (v0) for "ferrybox-name1" on "2022-11-11Z":
+// ferrybox-name1/v0/p1d/2022/2022-11/ferrybox-name1_2022-11-11.ndjson
+export const pathP1D = ({
+  endpoint = "",
+  isodate = new Date().toJSON().split("T")[0],
+  format = "ndjson",
+}) => {
+  isGregorianISODate(isodate);
+  const { dataVersion, durationSpecifier } = ferryboxOptions;
+  const [year, month] = isodate.split("-").slice(0, 2);
+  const isomonth = `${year}-${month}`;
+  return `${dataVersion}/${durationSpecifier}/${year}/${isomonth}/${endpoint}_${isodate}.${format}`;
+};
+
+//Google:
+//https://googleapis.deno.dev/
---
+++ cloud_test.ts
@@ -0,0 +1,14 @@
+import { assertEquals } from "https://deno.land/std@0.162.0/testing/asserts.ts";
+import { pathP1D } from "./cloud.ts";
+
+Deno.test("Cloud path for P1D is v0/p1d/$year/$year-$month/$file", (): void => {
+  const expected = "v0/p1d/2022/2022-11/ferrybox-name1_2022-11-11.ndjson";
+  assertEquals(
+    pathP1D({
+      endpoint: "ferrybox-name1",
+      isodate: "2022-11-11",
+      format: "ndjson",
+    }),
+    expected,
+  );
+});
